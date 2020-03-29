b3e4d1eeeb81d52e2e45b2184912a5f8474317b4
malwinderkaur <malwinderkaur@MALWINDERKAUR>
2020-03-29 17:45:15 +0530

    first commit

---
+++ Assignment.UserService/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment.UserService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.UserService.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return new User
+            {
+                Name = "John",
+                Age = 23,
+                Email = "john.doe@google.com"
+            };
+        }
+    }
+}
